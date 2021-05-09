5ef1d1f0c4402245880e3fc7219673ff9267fe3a
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-05-09 20:20:34 +0530

    Added some Custom icons on PaymentScreen.

--- src/screens/cart/PaymentScreen.tsx
+++ src/screens/cart/PaymentScreen.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { DEVICE_WIDTH, PUBLISHABLE_STRIPE_KEY } from '../../../constants';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { DEVICE_WIDTH, INDIAN_RUPEE_SIGN, PUBLISHABLE_STRIPE_KEY } from '../../../constants';
 import AppErrorMessage from '../../components/UI/form/AppErrorMessage';
-import { Colors as MuiColors } from 'react-native-paper';
+import { Colors as MuiColors, RadioButton, Text, Title, Surface, Button } from 'react-native-paper';
 import { CreditCardInput } from 'react-native-credit-card-input';
 import AppButton from '../../components/UI/app/Button';
 import { createTokenRequest, CreditCard } from '../../services/checkout';
+import { PaymentMethodType } from '../../utils/types';
+import CartDetails from '../../components/UI/cart/CartDetails';
+import { useCartStore } from '../../store/cart';
+import { Colors } from '../../../constants/colors';
+import AuthenticProductsIcon from '../../icons/AuthenticProductsIcon';
+import SecurePaymentIcon from '../../icons/SecurePaymentIcon';
+import EasyReturnsIcon from '../../icons/EasyReturnsIcon';
 
 interface CardDetails {
 	cvc: string;
@@ -22,6 +29,11 @@ const initialValues = {
 };
 
 const PaymentScreen = () => {
+	const { totalAmount, itemCount, cartItems } = useCartStore();
+	const [
+		paymentType,
+		setPaymentType
+	] = useState<PaymentMethodType>('CreditCard');
 	const [
 		cardDetails,
 		setCardDetails
@@ -43,7 +55,7 @@ const PaymentScreen = () => {
 			expiry: form.values.expiry
 		});
 	};
-	const handleCheckout = async () => {
+	const handlePlaceOrderForCreditCard = async () => {
 		const expiry = cardDetails.expiry.split('/');
 		const card: CreditCard = {
 			number: cardDetails.number,
@@ -56,16 +68,82 @@ const PaymentScreen = () => {
 		const info = await createTokenRequest(card);
 		console.log(info);
 	};
+	const handlePlaceOrderForCOD = () => {};
 	return (
 		<View style={styles.container}>
-			<CreditCardInput
-				validColor={MuiColors.green500}
-				onChange={handleChange}
-				requiresName
-				inputContainerStyle={styles.inputContainerStyle}
-			/>
-			<AppErrorMessage style={{ alignSelf: 'center' }} errorMessage={error} visible={error !== ''} />
-			<AppButton disabled={error !== ''} title="Checkout" onPress={handleCheckout} />
+			<ScrollView>
+				<CartDetails title="Order Details" totalAmount={totalAmount()} totalItems={itemCount()} />
+				<View style={styles.radioContainer}>
+					<Title style={styles.title}>Select Payment Method</Title>
+					<RadioButton.Group onValueChange={(newValue: any) => setPaymentType(newValue)} value={paymentType}>
+						<View style={styles.row}>
+							<RadioButton value="CreditCard" />
+							<Text>Credit Card</Text>
+						</View>
+						<View style={styles.row}>
+							<RadioButton value="COD" />
+							<Text>Cash On Delivery</Text>
+						</View>
+					</RadioButton.Group>
+				</View>
+				{paymentType === 'CreditCard' && (
+					<React.Fragment>
+						<CreditCardInput
+							validColor={MuiColors.green500}
+							onChange={handleChange}
+							requiresName
+							inputContainerStyle={styles.inputContainerStyle}
+						/>
+						<AppErrorMessage style={{ alignSelf: 'center' }} errorMessage={error} visible={error !== ''} />
+					</React.Fragment>
+				)}
+				<Surface
+					style={[
+						styles.surface,
+						styles.row,
+						{ justifyContent: 'space-between', marginVertical: 20 }
+					]}
+				>
+					<View style={styles.iconContainer}>
+						<AuthenticProductsIcon style={styles.icon} height={50} width={50} />
+						<Text style={styles.icon}>Authenticated Products</Text>
+					</View>
+					<View style={styles.iconContainer}>
+						<SecurePaymentIcon style={styles.icon} height={50} width={50} />
+						<Text style={styles.icon}>Secure Payments</Text>
+					</View>
+					<View style={styles.iconContainer}>
+						<EasyReturnsIcon style={styles.icon} height={50} width={50} />
+						<Text style={styles.icon}>Easy Returns</Text>
+					</View>
+				</Surface>
+			</ScrollView>
+			{cartItems.length > 0 && (
+				<Surface style={styles.surface}>
+					<View style={styles.totalAmount}>
+						<Title>
+							{INDIAN_RUPEE_SIGN}
+							{totalAmount()}
+						</Title>
+						{/* <Pressable onPress={handleViewCartDetails}>
+								<Text style={{ color: Colors.primary, fontWeight: 'bold' }}>View price details</Text>
+							</Pressable> */}
+					</View>
+					<Button
+						disabled={error !== '' && paymentType === 'CreditCard'}
+						mode="contained"
+						onPress={
+
+								paymentType === 'COD' ? handlePlaceOrderForCOD :
+								handlePlaceOrderForCreditCard
+						}
+						style={{ alignSelf: 'center' }}
+						color={Colors.accent}
+					>
+						place order
+					</Button>
+				</Surface>
+			)}
 		</View>
 	);
 };
@@ -73,10 +151,57 @@ const PaymentScreen = () => {
 export default PaymentScreen;
 
 const styles = StyleSheet.create({
-	container: {},
+	icon:
+		{
+			alignSelf: 'center',
+			textAlign: 'center'
+		},
+	container:
+		{
+			flex: 1
+		},
 	inputContainerStyle:
 		{
 			width: DEVICE_WIDTH * 0.9,
 			alignSelf: 'center'
+		},
+	radioContainer:
+		{
+			padding: 10,
+			marginVertical: 20
+		},
+	row:
+		{
+			flexDirection: 'row',
+			alignItems: 'center'
+		},
+	title:
+		{
+			alignSelf: 'center',
+			fontSize: 25,
+			marginVertical: 20
+		},
+	surface:
+		{
+			width: DEVICE_WIDTH,
+			flexDirection: 'row',
+			justifyContent: 'space-between',
+			padding: 10,
+			marginBottom: 5,
+			shadowColor: '#000',
+			shadowOffset:
+				{
+					width: 0,
+					height: 2
+				},
+			shadowOpacity: 0.25,
+			shadowRadius: 3.84,
+
+			elevation: 5
+		},
+	totalAmount: {},
+	iconContainer:
+		{
+			width: 100
 		}
 });
